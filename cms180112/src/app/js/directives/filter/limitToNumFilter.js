5ddf0e7c65c2e50a9b0369b8c352f0df9b64409a
wangjia0525 <453443367@qq.com>
2018-03-27 13:55:07 +0800

    想

--- cms180112/src/app/js/directives/filter/limitToNumFilter.js
+++
@@ -1,8 +0,0 @@
-App.filter('limitToNum', function() {
-    return function(input, num) {
-        if(input.length>num){
-            input = input.substring(0,num)+"...";
-        }
-        return input;
-    };
-});
--- cms180112/src/app/js/service/factory/productBuyFactory.js
+++
@@ -1,9 +0,0 @@
-//(c) Copyright 2016 Administrator. All Rights Reserved. 2016-05-27
-App.factory('productBuyFactory',function(SessionService){
-    var getProductBuy = function(params, callback, failCallback) {
-        return SessionService.requestGet("getProductByDate",params, callback, failCallback);
-    };
-    return {
-        getProductBuy 	: getProductBuy
-    };
-});
